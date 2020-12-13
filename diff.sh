3e0be230e06717be870b9813ce7c9d548efd06c9
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2020-12-13 02:06:38 +0100

    adds file to check if configs changed

---
+++ diff.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+files=( 
+    "vim/init.vim, ~/.config/nvim" 
+    ".zshenv, ~"
+    ".zshrc, ~"
+)
+
+
+updated_in_repo=()
+updated_on_disk=()
+for paths in "${files[@]}"; do
+    repo_path=$(echo ${paths} | cut -d "," -f 1 | tr -d '[:space:]')
+    disk_path=$(echo ${paths} | cut -d "," -f 2 | tr -d '[:space:]')/$(basename $repo_path)
+    disk_path=${disk_path/#\~/$HOME} #replace tilde with current home folder
+
+    if cmp --silent $repo_path $disk_path ;
+    then
+        continue
+    fi
+
+	printf '${repo_path} differs from ${disk_path}, diff:'
+	diff $repo_path $disk_path
+done
