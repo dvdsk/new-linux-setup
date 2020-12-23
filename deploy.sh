da8497e2f38ac5c2146e7cdbab491f75f945a6f8
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2020-12-23 13:43:18 +0100

    bugfix in deploy folder creation

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,9 @@ files=(
 
 for paths in "${files[@]}"; do
     repo_path=$(echo ${paths} | cut -d "," -f 1 | tr -d '[:space:]')
-    disk_folder=$(echo ${paths} | cut -d "," -f 2 | tr -d '[:space:]')/$(basename $repo_path)
-    disk_path=${disk_folder/#\~/$HOME} #replace tilde with current home folder
+    disk_folder=$(echo ${paths} | cut -d "," -f 2 | tr -d '[:space:]')
+    disk_folder=${disk_folder/#\~/$HOME} #replace tilde with current home folder
+	disk_path=$disk_folder/$(basename $repo_path)
 
 	[ -f $disk_path ] && mv $disk_path "${disk_path}.backup"
 	[ -f $disk_folder ] || mkdir -p $disk_folder
