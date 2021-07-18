965c3da3e8f902165656bc35f7e0001ade585c0e
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-07-18 18:31:51 +0200

    adds script to clean up extra zfs snapshots

---
+++ bin/clean_snapshots.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -e
+
+# This script will find a range of snapshots to destroy to free
+# the needed amount of free space, it will then delete these 
+# if you choose to continue
+#
+# A good alternative might be to list snapshots on used disk space using:
+# zfs list -o name,used -s used -t snap
+# using zfs destroy <NAME> 
+
+# sorted oldest to newest
+echo "indexing snapshots.... (this might take quite a while)"
+snapshots=`zfs list -t snapshot -o name | grep USERDATA/$USER`
+lines=`echo "$snapshots" | wc -l`
+
+
+NEEDED=$1
+[ -n "$NEEDED" ] || read -r -p "needed space in GB: " NEEDED
+NEEDED=$(($NEEDED * 1000000000))
+
+i=1
+reclaims=0
+while [ $reclaims -le $NEEDED ]; do
+	if [ $i -ge $lines ]; then
+		break
+	fi
+
+	first=`echo "$snapshots" | head -n 1`
+	last=`echo "$snapshots" | head -n $i | tail -n 1`
+	range="$first%$(echo $last | cut -d @ -f 2)"
+	# use nv to dry  walk
+	reclaimed=`zfs destroy -pnv $range | tail -n 1`
+	# can not pipe directly for some reason
+	reclaims=`echo $reclaimed | tr -s ' '| cut -d " " -f 2`
+	i=$((i+1))	
+done
+
+echo can reclaim $(($reclaims/1000000000)) GB by removing all snapshots between:
+echo -e '\t' $(zfs get creation $first | tail -n 1 | cut -d " " -f 4-10)
+echo -e '\t' $(zfs get creation $last | tail -n 1 | cut -d " " -f 4-10)
+
+PROCEED="n"
+read -r -p "proceed? (y/N): " PROCEED
+if [ "$PROCEED" = "y" ]; then
+	sudo zfs destroy $range
+	echo removed snapshots
+else 
+	echo cancelled
+fi
+
