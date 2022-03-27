11b5c5eae1e19bbc7d83236d9cdf45fa5d3f4fa9
dvdsk <noreply@davidsk.dev>
2022-03-27 18:50:38 +0200

    fixes to texlive setup

--- setup/texlive.sh
+++ setup/texlive.sh
@@ -7,21 +7,20 @@ sudo apt-get --quiet --yes install perl wget
 curr_year=$(date +'%Y')
 
 # remove any old texlive
-for year in {2020..$(($curr_year - 1))}; do
-	rm -rf /usr/local/texlive/$year
-	rm -rf ~/.texlive/$year
+for ((year = 2018; year < curr_year; year++)); do
+	sudo rm -rf /usr/local/texlive/$year
+	sudo rm -rf ~/.texlive/$year
 done
 
 # install fresh texlive
-TEMP=/tmp/texlive
-mkdir -p $TEMP
+TEMP=`mktemp --directory --suffix="_TEXLIVE"`
 wget --output-document $TEMP/install-tl.zip \
 	http://mirror.ctan.org/systems/texlive/tlnet/install-tl.zip
 unzip -o $TEMP/install-tl.zip -d $TEMP/install-tl
 path=$(ls $TEMP/install-tl | sort | tail -n 1)
 sudo perl $TEMP/install-tl/$path/install-tl <<< I
 
-# add simlink so path need not be changed between updates
+# # add simlink so path need not be changed between updates
 cd /usr/local/texlive
 current=$(find . 20* -maxdepth 0 -type d | sort | tail -n 1)
 cd -
@@ -29,5 +28,5 @@ cd -
 sudo rm /usr/local/texlive/installed
 sudo ln -s /usr/local/texlive/$current /usr/local/texlive/installed
 
-# clean up
+# # clean up
 rm -rf $TEMP
