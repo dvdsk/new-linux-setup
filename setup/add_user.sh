e31293d64ec777bc52cefd3cb2e80c804958df35
dvdsk <noreply@davidsk.dev>
2022-05-09 10:24:43 +0200

    add zfs datasets for important folders

--- setup/add_user.sh
+++ setup/add_user.sh
@@ -1,6 +1,10 @@
 USER=work
 
-sudo zfs create rpool/USERDATA/$USER
-sudo zfs set mountpoint=/home/$USER /rpool/USERDATA/$USER
+sudo zfs create home/$USER
+sudo zfs set mountpoint=/home/$USER home/$USER
+
+sudo zfs create home/$USER//Documents
+sudo zfs create home/$USER//Downloads
+sudo zfs create home/$USER//tmp
 
 sudo adduser --home /home/$USER --shell $(which zsh) --no-create-home $USER
