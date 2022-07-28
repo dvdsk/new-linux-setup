4a983786055865cc6f8410d4d88bbba12ce487f3
dvdsk <noreply@davidsk.dev>
2022-07-28 13:17:19 +0200

    setup comment plugin

--- vim/lua/text_tools.lua
+++ vim/lua/text_tools.lua
@@ -1,10 +1,1 @@
--- which key
-
-require("kommentary.config").configure_language("default", {
-	prefer_single_line_comments = true,
-})
-
--- require("telescope").load_extension("prosesitter")
--- require("prosesitter"):setup({
--- 	auto_enable = true, -- default true
--- })
+require("Comment").setup()
