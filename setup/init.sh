84f1cf64487a5b9b7b45ad3756b7a7d16b22ce48
David Kleingeld <git@davidsk.dev>
2025-01-18 13:15:51 +0100

    improve tool/program setup/remove outdated unused

--- setup/init.sh
+++ setup/init.sh
@@ -22,22 +22,13 @@ install_rust_and_tools() {
 		"du-dust"     # folder disk space, use: dust
 		"git-delta"   # diff replacement with syntax highlighting
 		"difftastic"  # diff replacement comparing on syntax
-		"rbtw"        # reboot to windows
 	)
 
 	cargo=$(ensure_cargo)
 	$cargo install "${tools[@]}"
-}
 
-install_gnome_specific_tools() {
-	sudo apt install gnome-tweaks gnome-shell-pomodoro
-}
-
-remove_snaps() {
-	if command -v snap &> /dev/null
-	then
-		sudo snap remove firefox
-	fi
+	rustup toolchain add nightly
+	$cargo +nightly install rbtw # reboot to windows
 }
 
 add_flatpack() {
@@ -46,9 +37,7 @@ add_flatpack() {
 	flatpak install com.discordapp.Discord com.github.wwmm.easyeffects org.telegram.desktop org.darktable.Darktable com.github.vikdevelop.photopea_app
 }
 
-remove_snaps
 add_flatpack
 install_basics_from_package_manager
 install_rust_and_tools
-install_gnome_specific_tools
 get_zsh_plugin_manager
--- setup/ui.sh
+++ setup/ui.sh
@@ -9,11 +9,23 @@ sudo apt install sway redshift lxpolkit
 # install bar
 cargo=$(ensure_cargo)
 
-$cargo install --git https://github.com/danieldg/rwaybar
-$cargo install --git https://github.com/j0ru/kickoff-dot-desktop
-$cargo install kickoff
+if ! which rwaybar; then
+	sudo apt install libfontconfig1-dev
+	$cargo install --git https://github.com/danieldg/rwaybar
+fi
 
-curl -L https://github.com/evavh/break-enforcer-s/releases/latest/download/break-enforcer_x86_64 --output break-enforcer
-chmod +x break-enforcer
-./break-enforcer install -w 15m -b 7m -l 30s -t
-rm break-enforcer
+if ! which kickoff-dot-desktop; then
+	sudo apt install libxkbcommon-dev
+	$cargo install --git https://github.com/j0ru/kickoff-dot-desktop
+fi
+
+if ! which kickoff; then
+	$cargo install kickoff
+fi
+
+if ! which break-enforcer; then
+	curl -L https://github.com/evavh/break-enforcer-s/releases/latest/download/break-enforcer_x86_64 --output break-enforcer
+	chmod +x break-enforcer
+	./break-enforcer install -w 15m -b 5m -l 30s -t
+	rm break-enforcer
+fi
