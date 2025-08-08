86816f2a9dce38a708f3c7c392bda78213a8c798
David Kleingeld <git@davidsk.dev>
2025-08-08 17:25:32 +0200

    fixes audio in sway + add screenshot to launcher

--- bin/launch_app.sh
+++ bin/launch_app.sh
@@ -7,5 +7,12 @@ export PATH="$PATH:$HOME/.cargo/bin"
 flatpaks=$(flatpak list --app --columns=name --columns=application | tail -n +1 | sed 's/\t\(.\+\)/ = flatpak run \1/') 
 snaps=$(snap list | awk '($1 != "Name") {print $1 "= snap run " $1;}')
 desktops=$(kickoff-dot-desktop)
+screenshot="\
+	Area to clipboard = ~/bin/screenshot.sh area clipboard
+	Area to file = ~/bin/screenshot.sh area file
+	Fullscreen to clipboard = ~/bin/screenshot.sh fullscreen clipboard
+	Fullscreen to file = ~/bin/screenshot.sh fullscreen file
+	Edit clipboard image = wl-paste | swappy -f -
+"
 
-echo "${flatpaks}${snaps}${desktops}" | kickoff --from-stdin
+echo "${screenshot}${flatpaks}${snaps}${desktops}" | kickoff --from-stdin
